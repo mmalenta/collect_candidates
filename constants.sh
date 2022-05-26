4342b249d1925fb2923bb2b7f6f192b91154761c
Mateusz Malenta <mateusz.malenta@gmail.com>
2022-05-26 13:54:42 +0100

    Fix(cosntants): add the missing constants.sh file

---
+++ constants.sh
@@ -0,0 +1,6 @@
+OUTPUT_DIR="/state/partition1/node_controller/output"
+STORAGE_DIR="/storage/frbid_cands"
+
+# Print out error message if we have more than 5% of candidates
+# not processed correctly. Print out an error otherwise
+ERROR_THRESHOLD=0.05
